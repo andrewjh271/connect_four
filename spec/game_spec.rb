01fd62d45a9291cda56d7050f80dfb6b530a17f2
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-06-13 15:16:02 -0700

    game_spec.rb works when run as part of group

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,34 +5,48 @@ describe Game do
 
   attr_reader :board, :player
 
-  subject { Game.new }
-  before { @player = double(:player, name: 'Andrew', first?: true) }
+  # subject { Game.new }
+  before do
+    allow($stdout).to receive(:write)
+    @player = double(:player, name: 'Andrew', first?: true)
+  end
 
   describe '#query_move' do
-    it "calls Board#move(player, 'c') for user input 'c'" do
-      allow(subject).to receive(:gets).and_return('c')
-      expect { subject.query_move(player) }.to output(
+    it 'prompts for user input and returns it' do
+      # This line works if I call rspec on this specific file, but not if I call rspec files
+      # as a group. (Having to do with Kernel reading from where file was called).
+      # allow(subject).to receive(:gets).and_return('c')
+      allow_any_instance_of(Kernel).to receive(:gets).and_return('c')
+      # Using subject with the above Kernel mock causes expectation to read output on instantiation.
+      game = Game.new
+      expect { game.query_move(player) }.to output(
         "Select column for Andrew's move: "
       ).to_stdout
       expect(subject.query_move(player)).to eq 'c'
     end
 
     it 'does not accept invalid columns' do
-      allow(subject).to receive(:gets).and_return('h', 'g')
-      expect { subject.query_move(player) }.to output(
+      # allow_any_instance_of does not support multiple returns, but adding it allows me to run
+      # rspec files as a group (whether I use Object or Kernel doesn't seem to matter here)
+      allow_any_instance_of(Object).to receive(:gets).and_return('*not_read*')
+      game = Game.new
+      allow(game).to receive(:gets).and_return('h', 'g')
+      expect { game.query_move(player) }.to output(
         "Select column for Andrew's move: " \
         "\e[1A" \
         "\e[2K" \
         "\e[31mSelect column for Andrew's move (e.g., b): \e[0m"
       ).to_stdout
-      expect(subject.query_move(player)).to eq 'g'
+      expect(game.query_move(player)).to eq 'g'
     end
   end
 
   describe '#query_name' do
     it 'asks for name and returns response' do
-      allow(subject).to receive(:gets).and_return('Andrew')
-      expect(subject.query_name(1)).to eq('Andrew')
+      # allow(subject).to receive(:gets).and_return('Andrew')
+      allow_any_instance_of(Object).to receive(:gets).and_return('Andrew')
+      game = Game.new
+      expect(game.query_name(1)).to eq('Andrew')
     end
   end
 end
